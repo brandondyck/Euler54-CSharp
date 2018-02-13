565b09d0bc3ec60f382531ebc7ffec14fec2df37
Brandon Dyck <brandon@dyck.us>
2018-02-13 01:45:54 -0700

    Added tests for PokerScore comparison

---
+++ Euler54Tests/PokerScoreTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler54_CSharp;
+
+namespace Euler54Tests
+{
+    [TestClass]
+    public class PokerScoreTests
+    {
+        [TestMethod]
+        public void IdenticalHandsAreADraw()
+        {
+            var player1 = new PokerScore(Rank.Flush, Value.Ten, Value.Three);
+            var player2 = new PokerScore(Rank.Flush, Value.Ten, Value.Three);
+            Assert.AreEqual(0, player1.CompareTo(player2));
+        }
+
+        [TestMethod]
+        public void StrongerRankWins()
+        {
+            var player1 = new PokerScore(Rank.Straight, Value.Six, Value.Six);
+            var player2 = new PokerScore(Rank.HighCard, Value.A, Value.A);
+            Assert.IsTrue(player1.CompareTo(player2) > 0);
+        }
+
+        [TestMethod]
+        public void HighestInRankWinsOnTiedRank()
+        {
+            var player1 = new PokerScore(Rank.Straight, Value.Six, Value.A);
+            var player2 = new PokerScore(Rank.Straight, Value.Ten, Value.J);
+            Assert.IsTrue(player1.CompareTo(player2) < 0);
+        }
+
+        [TestMethod]
+        public void HighestInHandBreaksTie()
+        {
+            var player1 = new PokerScore(Rank.FourOfAKind, Value.Five, Value.A);
+            var player2 = new PokerScore(Rank.FourOfAKind, Value.Five, Value.K);
+            Assert.IsTrue(player1.CompareTo(player2) > 0);
+        }
+    }
+}
---
+++ Euler54Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Euler54Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Euler54Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8847929b-47b5-4e3f-ae01-bc5ba3f4030b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
